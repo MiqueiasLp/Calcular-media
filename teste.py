f8495c69ff93b8cccb4bcde3171389ec55d73b92
MiqueiasLp <miqueiaspinheirolopes@gmail.com>
2026-08-29 19:50:18 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -8,19 +8,19 @@ def calcular_notas(qtd_notas):
         contador += 1
     return total / qtd_notas
 
-def verificar_aprovacao(media, faltas, nome):
+def verificar_aprovacao(media, faltas):
     if media >= 7 and faltas <= 5:
-        print(f"{nome} foi aprovado com média {media} e {faltas} faltas.")
         return "Aprovado"
     elif media >= 5 and faltas <= 5:
-        print(f"{nome} está em recuperação com média {media} e {faltas} faltas.") 
         return "Em Recuperação"
     else:
-        print(f"{nome} está reprovado com média {media} e {faltas} faltas.")
         return "Reprovado"
 
 n = input("Digite o nome do aluno: ")
 f = int(input("Digite a quantidade de faltas: "))
 qtd = int(input("Digite a quantidade de notas: "))
+
 media = calcular_notas(qtd)
-verificar_aprovacao(media, f, n)
+status = verificar_aprovacao(media, f)
+
+print(f"\nO aluno {n} teve a média {media:.2f} e o resultado: {status}")
